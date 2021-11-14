b74458701d683832ba1a463031038065eaff308a
Abhishek <abbansal25@gmail.com>
2021-11-14 17:21:05 +0530

    bug fixed, db schema only supports movie name and description

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,10 +5,10 @@ import { List } from "semantic-ui-react";
 
 export default function Movie() {
   let { id } = useParams();
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState([]);
 
   const getMovie = () => {
-    axios.get(`http://localhost:5000/streams/${id}`).then((res) => {
+    axios.get(`http://localhost:5000/movie/${id}`).then((res) => {
       console.log(res,"movie data");
       setMovie(res.data);
     });
@@ -17,17 +17,17 @@ export default function Movie() {
   useEffect(() => {
     getMovie();
   }, []);
-  console.log(movie, "movie arr");
+  console.log(movie, "movie obj");
   return (
     <div style={{ paddingTop: "20px" }}>
       <List>
         <List.Item>
           <List.Header>
-            <h2>{movie.movie}</h2>
+            <h2>{movie[0].movie}</h2>
           </List.Header>
           <List.Header>Release year</List.Header>
           {movie.releaseDate}
-          <List.Header>{movie.description}</List.Header>
+          <List.Header>{movie[0].description}</List.Header>
           <List.Header>Actors</List.Header>
           {movie.actors}
           <List.Header>Producer</List.Header>
